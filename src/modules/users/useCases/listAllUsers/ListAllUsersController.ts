35c9c0b803cb318dbbbb0eab73764585f6873b9b
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-03-28 22:08:01 -0300

    feat: Made list all users use case

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      if (Array.isArray(user_id)) {
+        throw Error("user_id must be a string");
+      }
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin) {
+      throw new Error("User not found");
+    }
+
+    if (userAdmin.admin === false) {
+      throw new Error("User is not a admin");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
